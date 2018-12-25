be762762bfe5ea32406cfa59b5e676390f4e26ad
Vicente Valls Rios <vicente-valls@users.noreply.github.com>
2018-12-25 13:46:18 +0100

    adding create task (#4)

---
+++ src/validator/IsObjectValidator.ts
@@ -0,0 +1,22 @@
+import {registerDecorator, ValidationArguments, ValidationOptions} from 'class-validator';
+
+export function IsObject(validationOptions?: ValidationOptions) {
+    return function (object: Object, propertyName: string) {
+        registerDecorator({
+            name: 'IsObject',
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            validator: {
+                validate(value: any) {
+                    return typeof value === 'object';
+                },
+                defaultMessage: defaultMessage
+            }
+        });
+    };
+}
+
+function defaultMessage(validationArguments?: ValidationArguments): string {
+    return `${validationArguments.targetName}.${validationArguments.property} is not valid`;
+}
