0adf63e34abfc70cbe8d6c6d647adf867acb17d3
Vicente Valls Rios <vicente-valls@users.noreply.github.com>
2019-01-01 21:17:25 +0100

    new error response

---
+++ src/service/CustomValidator.ts
@@ -0,0 +1,43 @@
+import SYMBOLS from '../dependency-injection/Symbols';
+import { provide } from "inversify-binding-decorators";
+import {inject} from 'inversify';
+import {ValidationError, Validator} from 'class-validator';
+import {ErrorItem} from '../error/ErrorItem';
+import {InvalidDtoError} from '../error/InvalidDtoError';
+import {Source} from '../error/Source';
+
+@provide(SYMBOLS.CustomValidator)
+export class CustomValidator {
+    constructor(@inject(SYMBOLS.ClassValidator) private classValidator: Validator) {
+    }
+
+    public validateSync(object: Object): void {
+        const validationErrors: ValidationError[] = this.classValidator.validateSync(object);
+        if (validationErrors.length > 0) {
+            const errorItems = this.mapValidationErrors(validationErrors, [], '/');
+            throw new InvalidDtoError(errorItems, 'invalid data');
+        }
+    }
+
+    private mapValidationErrors(
+        validationErrors: ValidationError[], errorItems: ErrorItem[], errorPointerPrefix: string
+    ): ErrorItem[] {
+        validationErrors.forEach((validationError) => {
+            for (const constraintType in validationError.constraints) {
+                const title = validationError.constraints[constraintType];
+                const pointer = errorPointerPrefix + validationError.property;
+                const source = new Source(pointer, null);
+                errorItems.push(new ErrorItem(title, source));
+            }
+            if (validationError.children) {
+                return this.mapValidationErrors(
+                    validationError.children,
+                    errorItems,
+                    `${errorPointerPrefix}${validationError.property}/`
+                );
+            }
+        });
+
+        return errorItems;
+    }
+}
--- tests/unit/dto/CreateTaskSpec.ts
+++ tests/unit/dto/CreateTaskSpec.ts
@@ -6,14 +6,20 @@ import {ClassTransformer} from 'class-transformer';
 import {given} from 'mocha-testdata';
 import {CreateTask} from '../../../src/dto/CreateTask';
 import * as glob from 'glob';
+import {CustomValidator} from '../../../src/service/CustomValidator';
+import {ErrorItem} from '../../../src/error/ErrorItem';
 
+interface IInvalidFixture {
+    createTask: Object|CreateTask;
+    expectedValidationResult: ErrorItem[];
+}
 suite('CreateTaskSpec', () => {
     const fixtureFolderName = 'create_task';
 
     const validFixtureFilenames: string[] = glob.sync(__dirname + `/fixtures/${fixtureFolderName}/valid/*.json`);
     const invalidFixtureFilenames: string[] = glob.sync(__dirname + `/fixtures/${fixtureFolderName}/invalid/*.json`);
     const classTransformer = new ClassTransformer();
-    const classValidator = new Validator();
+    const classValidator: CustomValidator = new CustomValidator(new Validator());
 
     given(validFixtureFilenames).test('Testing valid with ...', testSuccessValidation);
     given(invalidFixtureFilenames).test('Testing invalid with ...', testFailValidation);
@@ -21,16 +27,17 @@ suite('CreateTaskSpec', () => {
     function testSuccessValidation(filename) {
         const fixture = require(filename);
         const model = classTransformer.plainToClass<CreateTask, Object>(CreateTask, fixture);
-        return classValidator.validate(model).then(errors => {
-            assert.strictEqual(errors.length, 0);
-        })
+        return classValidator.validateSync(model);
     }
 
     function testFailValidation(filename) {
-        const fixture = require(filename);
-        const model = classTransformer.plainToClass<CreateTask, Object>(CreateTask, fixture);
-        return classValidator.validate(model).then(errors => {
-            assert.ok(!!errors.length);
-        });
+        const fixture: IInvalidFixture = require(filename);
+        const model = classTransformer.plainToClass<CreateTask, Object>(CreateTask, fixture.createTask);
+        try {
+            classValidator.validateSync(model);
+        } catch (invalidDtoError) {
+            return assert.deepEqual(invalidDtoError.errors, fixture.expectedValidationResult);
+        }
+        assert.fail('unexpected invalid model passed validation');
     }
 });
